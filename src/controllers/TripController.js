43b741f0aa2e08d16923f3c453bf67dccedd571b
Анастасия <na.konsta@gmail.com>
2019-09-01 16:31:04 +0300

    Variable instead of method call and sorting

--- src/controllers/TripController.js
+++ src/controllers/TripController.js
@@ -1,63 +1,108 @@
 import {TripEvent} from '../components/tripEvent.js';
 import {TripEventEdit} from '../components/tripEventEdit.js';
+import {Sort} from '../components/sort.js';
 import {render, Positions} from '../utils.js';
 
 class TripController {
   constructor(container, tripEvents) {
     this._container = container;
     this._tripEvents = tripEvents;
+    this._sort = new Sort();
   }
 
   init() {
+    render(document.querySelector(`.trip-events`), this._sort.getElement(), Positions.AFTERBEGIN);
     this._tripEvents.forEach((tripEvent) => this._renderTripEvent(tripEvent));
+
+    this._sort.getElement().addEventListener(`click`, (evt) => {
+      this._onSortClick(evt);
+    });
+
   }
 
   _renderTripEvent(tripEventsMockParam) {
     const tripEvent = new TripEvent(tripEventsMockParam);
     const tripEventEdit = new TripEventEdit(tripEventsMockParam);
 
+    const tripEventElement = tripEvent.getElement();
+    const tripEventEditElement = tripEventEdit.getElement();
+
     const onEscKeyDown = (evt) => {
       if (evt.key === `Escape` || evt.key === `Esc`) {
-        this._container.replaceChild(tripEvent.getElement(), tripEventEdit.getElement());
+        this._container.replaceChild(tripEventElement, tripEventEditElement);
         document.removeEventListener(`keydown`, onEscKeyDown);
       }
     };
 
-    tripEvent.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, () => {
-      this._container.replaceChild(tripEventEdit.getElement(), tripEvent.getElement());
+    tripEventElement.querySelector(`.event__rollup-btn`).addEventListener(`click`, () => {
+      this._container.replaceChild(tripEventEditElement, tripEventElement);
       document.addEventListener(`keydown`, onEscKeyDown);
     });
 
-    tripEventEdit.getElement().querySelector(`.event__input--destination`).addEventListener(`focus`, () => {
+    tripEventEditElement.querySelector(`.event__input--destination`).addEventListener(`focus`, () => {
       document.removeEventListener(`keydown`, onEscKeyDown);
     });
 
-    tripEventEdit.getElement().querySelectorAll(`.event__input--time`).forEach((it) => it.addEventListener(`focus`, () => {
+    tripEventEditElement.querySelectorAll(`.event__input--time`).forEach((it) => it.addEventListener(`focus`, () => {
       document.removeEventListener(`keydown`, onEscKeyDown);
     }));
 
-    tripEventEdit.getElement().querySelector(`.event__input--price`).addEventListener(`focus`, () => {
+    tripEventEditElement.querySelector(`.event__input--price`).addEventListener(`focus`, () => {
       document.removeEventListener(`keydown`, onEscKeyDown);
     });
 
-    tripEventEdit.getElement().querySelector(`.event__input--destination`).addEventListener(`blur`, () => {
+    tripEventEditElement.querySelector(`.event__input--destination`).addEventListener(`blur`, () => {
       document.addEventListener(`keydown`, onEscKeyDown);
     });
 
-    tripEventEdit.getElement().querySelectorAll(`.event__input--time`).forEach((it) => it.addEventListener(`blur`, () => {
+    tripEventEditElement.querySelectorAll(`.event__input--time`).forEach((it) => it.addEventListener(`blur`, () => {
       document.addEventListener(`keydown`, onEscKeyDown);
     }));
 
-    tripEventEdit.getElement().querySelector(`.event__input--price`).addEventListener(`blur`, () => {
+    tripEventEditElement.querySelector(`.event__input--price`).addEventListener(`blur`, () => {
       document.addEventListener(`keydown`, onEscKeyDown);
     });
 
-    tripEventEdit.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, () => {
-      this._container.replaceChild(tripEvent.getElement(), tripEventEdit.getElement());
+    tripEventEditElement.querySelector(`.event__rollup-btn`).addEventListener(`click`, () => {
+      this._container.replaceChild(tripEventElement, tripEventEditElement);
       document.removeEventListener(`keydown`, onEscKeyDown);
     });
 
-    render(this._container, tripEvent.getElement(), Positions.BEFOREEND);
+    render(this._container, tripEventElement, Positions.BEFOREEND);
+  }
+
+  _onSortClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.tagName !== `LABEL`) {
+      return;
+    }
+
+    this._container.innerHTML = ``;
+
+    const allSorts = document.querySelectorAll(`.trip-sort__input`);
+    allSorts.forEach((sortInput) => sortInput.removeAttribute(`checked`));
+
+    switch (evt.target.dataset.sortType) {
+      case `time`:
+        const sortedByTimeTripEvents = this._tripEvents.slice().sort((a, b) => {
+          let aDuration = (a.timeEnd.hours * 60 + a.timeEnd.mins) - (a.timeStart.hours * 60 + a.timeStart.mins);
+          let bDuration = (b.timeEnd.hours * 60 + b.timeEnd.mins) - (b.timeStart.hours * 60 + b.timeStart.mins);
+          return bDuration - aDuration;
+        });
+        sortedByTimeTripEvents.forEach((tripEvent) => this._renderTripEvent(tripEvent));
+        document.querySelector(`#sort-time`).setAttribute(`checked`, `checked`);
+        break;
+      case `price`:
+        const sortedByPriceTripEvents = this._tripEvents.slice().sort((a, b) => b.price - a.price);
+        sortedByPriceTripEvents.forEach((tripEvent) => this._renderTripEvent(tripEvent));
+        document.querySelector(`#sort-price`).setAttribute(`checked`, `checked`);
+        break;
+      case `event`:
+        this._tripEvents.forEach((tripEvent) => this._renderTripEvent(tripEvent));
+        document.querySelector(`#sort-event`).setAttribute(`checked`, `checked`);
+        break;
+    }
   }
 }
 
